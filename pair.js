2a802cb44cd93cfe7d16549bf0b01a0406d4f81a
keith <157195350+Keithkeizzah@users.noreply.github.com>
2025-01-06 09:44:17 +0300

    Update pair.js

--- pair.js
+++ pair.js
@@ -93,7 +93,7 @@ router.get('/', async (req, res) => {
                         contextInfo: {
                             mentionedJid: [Pair_Code_By_Brasho_Kish.user.id], // Mention the sender in the audio message
                             externalAdReply: {
-                                title: 'Thanks for choosing Alpha Md happy deployment 💜',
+                                title: 'Thanks for choosing 𝗞𝗲𝗶𝘁𝗵 𝗦𝘂𝗽𝗽𝗼𝗿𝘁 happy deployment 💜',
                                 body: 'Regards Keithkeizzah',
                                 thumbnailUrl: 'https://i.imgur.com/vTs9acV.jpeg',
                                 sourceUrl: 'https://whatsapp.com/channel/0029Vaan9TF9Bb62l8wpoD47',
